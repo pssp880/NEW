965ae97330f30f4c6909b1bea84bea8f7f1ef55e
pssp880 <psspshiva@gmail.com>
2021-05-11 15:15:22 +0530

    Fourth

--- src/main/kotlin/while.kt
+++ src/main/kotlin/while.kt
@@ -1,9 +1,9 @@
 fun main() {
 
-    var i = 1
-
+    var i = 1                 // Declaring and initializing a variable (i)
+                              //The below code will loop until the condition becomes false
     while (i <= 5) {
-        println("Number $i")
-        ++i
+        println("Number $i") //prints the output
+        ++i                   // increments the value by 1
     }
 }
