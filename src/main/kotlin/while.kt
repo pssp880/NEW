04b1578b53caced98f5cabedf4ba3691f2cb5e90
pssp880 <psspshiva@gmail.com>
2021-05-17 12:01:43 +0530

    added user vs std functions

--- src/main/kotlin/while.kt
+++ src/main/kotlin/while.kt
@@ -1,9 +1,7 @@
-fun main() {
-
-    var i = 1                 // Declaring and initializing a variable (i)
-                              //The below code will loop until the condition becomes false
-    while (i <= 5) {
-        println("Number $i") //prints the output
-        ++i                   // increments the value by 1
-    }
+fun main(){
+    var i = 1
+    do{
+        println("Number $i")
+        ++i
+    }while (i <= 5)
 }
